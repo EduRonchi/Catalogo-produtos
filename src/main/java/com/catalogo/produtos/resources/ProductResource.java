fbd2fdf172b39bb210cd7c39f637492fcf385f2d
EduRonchi <financaronchi@gmail.com>
2022-08-09 21:26:12 -0300

    Service layer, component registration

--- src/main/java/com/catalogo/produtos/resources/ProductResource.java
+++ src/main/java/com/catalogo/produtos/resources/ProductResource.java
@@ -1,19 +1,34 @@
 package com.catalogo.produtos.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.catalogo.produtos.entities.Product;
+import com.catalogo.produtos.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductResource {
 
+	@Autowired
+	private ProductService service;
+	
 	@GetMapping
-	public ResponseEntity<Product> findAll() {
-		Product p = new Product(1L, "Shampoo", "Shampoo para cabelos oleosos.", 18.90);
-		return ResponseEntity.ok().body(p);
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Product obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
