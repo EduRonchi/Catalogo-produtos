99ffdaa2cff87ccc4534a1a2c34b27590bb5d00f
EduRonchi <financaronchi@gmail.com>
2022-08-11 15:30:17 -0300

    Exception handling - delete

--- src/main/java/com/catalogo/produtos/services/ProductService.java
+++ src/main/java/com/catalogo/produtos/services/ProductService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.catalogo.produtos.entities.Product;
@@ -30,7 +32,11 @@ public class ProductService {
 	}
 
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);		
+	} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+	}
 	}
 	
 	public Product update(Long id, Product obj) {
