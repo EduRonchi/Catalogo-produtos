3b7b5a31a717ea0cf6ac4b391cebb3cca7e79776
EduRonchi <financaronchi@gmail.com>
2022-08-11 14:34:38 -0300

    Exception handling - findById

--- src/main/java/com/catalogo/produtos/services/ProductService.java
+++ src/main/java/com/catalogo/produtos/services/ProductService.java
@@ -8,21 +8,21 @@ import org.springframework.stereotype.Service;
 
 import com.catalogo.produtos.entities.Product;
 import com.catalogo.produtos.repositories.ProductRepository;
+import com.catalogo.produtos.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;	
-		
-	
+			
 	public List<Product> findAll() {
 		return repository.findAll();
 	}
 	
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public Product insert(Product obj) {
